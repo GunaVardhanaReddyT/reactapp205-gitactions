04d229ee33e4ea0aa1657a1e67ccc390084366f8
GunaVardhanaReddyT <2300032314@kluniversity.in>
2025-07-28 10:02:36 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project - S205</h1>
+      <p>This is a simple homepage deployed on GitHUb pages using vite.</p>
+    </div>
+  );
+}
+
+export default App
