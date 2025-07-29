c452db11ec078e21a4431e3a8c2d0c4b4d9c9aad
GunaVardhanaReddyT <2300032314@kluniversity.in>
2025-07-29 12:06:28 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import "style.css";
+import "./style.css";
 
 function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - S205</h1>
       <p>CI/CD and Cloud DevOps</p>
-      <p>This is a simple homepage deployed on GitHub pages using vite.</p>
+      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
     </div>
   );
 }
